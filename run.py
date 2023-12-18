259c2280df7a51effa609bd589c0d77423dc0acf
aerienn <aerienn.14@gmail.com>
2023-12-18 17:16:38 +0800

    Added changes needed to make app ready for deployment

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
+import os
+from waitress import serve
 from app import app
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Check if the environment variable FLASK_ENV is set to 'development'
+    if os.environ.get('FLASK_ENV') == 'development':
+        # Run the development server
+        app.run(debug=True)
+    else:
+        # Run Waitress in production mode
+        serve(app, host="127.0.0.1", port=5000)
